90da134f6d1b17a4a078b0c174f24adcb82a483d
UTSProgrammingClub <utsprogramming@gmail.com>
2014-01-11 21:45:25 -0500

    Update Tower.py

--- Tower.py
+++ Tower.py
@@ -1,13 +1,19 @@
 """
 class Tower:
-@author: #PUT YOUR NAME HERE IF YOU WANT TO CLAIM THIS CLASS
+@author: Sam
 
 Parameters:
-range : int #How large its radius is in pixels. 
+attackRange : int #How large its radius is in pixels. 
 attackSpeed : int #Delay in milliseconds between shots. Multiples of 50. 
 bulletType : Bullet #The type of bullet that this fires. 
 
 Methods:
+__init__ (self, ...):
+    "Constructor"
+
 fire (self, target : Enemy) -> None:
     "Fires a bullet at %target"
+    
+update (self) -> None:
+    "Updates tower."
 """
